383c970a57f6adab2a25aab3c26faa05b57ac82b
aggarwalse@gmail.com <aggarwalse@gmail.com>
2018-03-24 23:23:27 -0400

    AngularJS Spring Boot Rest Project

---
+++ eventmanager/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import com.example.demo.dto.UserDto;
+import com.example.demo.service.UserService;
+
+@RequestMapping("/user")
+@RestController
+public class UserController {
+	@Autowired
+	UserService userService;
+	@RequestMapping("/getUser")
+	public UserDto getUserById(@PathVariable Integer userId) {
+		return userService.getUserById(userId);
+	}
+	@RequestMapping("getAllUsers")
+	public List < UserDto > getAllUsers() {
+		return userService.getAllUsers();
+	}
+	@RequestMapping(value = "/saveUser", method = RequestMethod.POST)
+	public void saveUser(@RequestBody UserDto userDto) {
+		userService.saveUser(userDto);
+	}
+}
