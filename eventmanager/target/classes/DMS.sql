dd13243e4133803a28bfffa506e94990e8227bb3
aggarwalse@gmail.com <aggarwalse@gmail.com>
2018-03-28 15:58:39 -0400

    Added DML

---
+++ eventmanager/target/classes/DMS.sql
@@ -0,0 +1,10 @@
+CREATE TABLE TMP_USER_DATA(
+    userId NUMBER,
+    userName VARCHAR(20)
+);
+
+CREATE TABLE TMP_SKILL_DATA(
+    skillId NUMBER,
+    skillName VARCHAR(20),
+    userId NUMBER
+);
